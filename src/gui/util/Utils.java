179dc73847090f0d54b0afdd5fae0559b2289e73
Matheus Felipe Trinca <matheusfelipetrinca@gmail.com>
2020-07-12 12:56:43 -0300

    Adicionado DialogForm do Department

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,14 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	// recebe o evento do mouse(clique) retorna o Stage do evento
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage)((Node) event.getSource()).getScene().getWindow();
+	}
+	
+}
