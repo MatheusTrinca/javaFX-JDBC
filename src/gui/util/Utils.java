5bee71478f70dcb764dba1e5fc1f03c41c21912a
Matheus Felipe Trinca <matheusfelipetrinca@gmail.com>
2020-07-13 12:42:41 -0300

    Salvando novo Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 
 public class Utils {
 
-	// recebe o evento do mouse(clique) retorna o Stage do evento
+	// recebe o evento do mouse(clique) retorna o Stage do event
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage)((Node) event.getSource()).getScene().getWindow();
 	}
